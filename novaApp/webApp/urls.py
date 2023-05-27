dea10442af06d571437ca862daba679198c8f1dd
DanielPalacios05 <drpalaciod@eafit.edu.co>
2023-05-27 13:48:32 -0500

    model working

--- novaApp/webApp/urls.py
+++ novaApp/webApp/urls.py
@@ -2,11 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('', index, name='index'),
-    
-=======
     path('', home, name='home'),
     path('base/', base, name='base'),
->>>>>>> origin
 ]
--- novaApp/webApp/views.py
+++ novaApp/webApp/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-import models
+
 
 # Create your views here.
 def home(request):
    return render(request, 'home.html')
+
+def base(request):
+   return render(request,"base.hmtl")
+
+def teachers(request,id_profesor):
+   pass
 
 
 
